38199bcc854c0b53cc8fb3710835f19ac8825ea5
yoojunsu <vcoavmv@naver.com>
2022-05-16 11:34:46 +0900

    회원가입 '베스트 지점장'텍스트 클릭시 베스트 지점장 리스트 영역으로 스크롤 이동 작업

--- src/js/join.js
+++ src/js/join.js
@@ -8,4 +8,11 @@ $(function(){
         slidesPerView: "auto",
         spaceBetween: 20,
     });
+
+    //베스트 지점장 텍스트 클릭시 영역 스크롤 이동
+    $("#scrHofObj").on("click",function(){
+        $("html,body").animate({
+            scrollTop: $(".best-leader-wrap").position().top - 100
+        },100);
+    });
 });
